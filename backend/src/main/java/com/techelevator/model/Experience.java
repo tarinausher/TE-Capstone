bb26144f2561bb4a099937924db7410ae191b1f4
Joseph <jbgriffithiv@gmail.com>
2021-12-06 21:09:33 -0500

    created Student, Experience, Portfolio and Degree models

---
+++ backend/src/main/java/com/techelevator/model/Experience.java
@@ -0,0 +1,9 @@
+package com.techelevator.model;
+
+public class Experience {
+    private String title;
+    private String organization;
+    private String dateStarted;
+    private String dateEnded;
+    private String description;
+}
