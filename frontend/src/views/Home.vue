88dcb6f6f80db80b87398429aa673cdb02ef76ed
Tarina Usher <tarinausher@gmail.com>
2021-12-07 20:45:59 -0500

    Why won't this merge error go away

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,29 +1,15 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-<<<<<<< HEAD
     <p>Welcome to the Student Profile Builder!</p>
     <p>Product in process.</p>
     <!-- <p>You must be authenticated to see this</p> -->
-    <div class= "search">
+    <div class= "search"> <!--Added by Josh--> 
       <input type="search" value="Search"/> 
       <input type="submit" value="Submit"/> 
       <input type="submit" value="Browse Roster" />
-=======
-    <p>You must be authenticated to see this</p>
-<!-- what i added is under this comment -->
-    <div class= "search">
-     <input type="search" value="search"/> <input type="submit" value="submit"> <input type="submit" value="Browse Roster" />
-    </div>
-
-    <div class= "">
-      
->>>>>>> b222578d59ff2452707c0cd2e0edbe4c0afc8283
     </div>
   </div>
-
-  
-
 </template>
 
 <script>
@@ -31,7 +17,3 @@ export default {
   name: "home"
 };
 </script>
-
-<style>
-
-</style>
