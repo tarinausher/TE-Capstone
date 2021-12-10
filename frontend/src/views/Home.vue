7469cb8707cf7c042321960dfe0d8c4d88b44925
Joshua Littles <littles.j.i.i@gmail.com>
2021-12-10 08:47:17 -0500

    frontend

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-<!-- what i added is under this comment -->
-    
-    <div class= "search">
-    <section id="banner">
-     <input type="search" value="search"/> <input type="submit" value="submit"> <input type="submit" value="Browse Roster" />
-    </section>
+    <div class="header">
+        <h1>Student Profile Builder</h1>
+      <p>You must be authenticated to see this</p>
+  <!-- what i added is under this comment -->
+    </div> 
+    <div class="search">
+      <section id="banner">
+        <input type="search" value="search"/> <input type="submit" value="submit"> <input type="submit" value="Invite +" />
+      </section>
     </div>
-
-     <div>
+    
+    
+  <div class="left-panel">
      <section id="left-panel">
        <nav>
          <menu>
@@ -35,8 +37,8 @@
      </section>
    </div>
 
-  <div>
-    <section id="center">
+  <div class="center-right">
+    <section id="center-right">
       <article id="student-profiles">
         <img src="https://via.placeholder.com/120" alt="placeholder">
            <li class="center-panel">
@@ -73,7 +75,45 @@
     </section>
   </div>
 
+  <div class="center-left">
+    <section id="center-left">
+      <article id="employer-profiles">
+        <img src="https://via.placeholder.com/120" alt="placeholder">
+           <li class="center-panel">
+             <a href='#'> Bio </a>
+           </li>
+
+           <li>
+             <a href='#'> Career experience </a>
+           </li>
+
+           <li>
+             <a href='#'> Educational Background </a>
+           </li>
+
+           <li>
+             <a href='#'> Soft Skills </a>
+           </li>
+
+           <li>
+             <a href='#'> Contact Preferences </a>
+           </li>
+
+           <li>
+             <a href='#'> Interests  </a>
+           </li>
+
+           <li>
+             <a href='#'> View Full Profile </a>
+           </li>
+      </article>
+    </section>
+    
   </div>
+  </div>
+ 
+
+  
 </template>
 
 <script>
@@ -83,24 +123,37 @@ export default {
 </script>
 
 <style>
+#section-left {
+  grid-area: section-left;
+}
 
-#banner {
+#center-left {
+  grid-area: center-left;
+}
+
+#center-right {
+  grid-area: center-right;
+}
+
+.header{
   grid-area: header;
 }
 
-section#left {
-  grid-area: left-panel;
+.search {
+  grid-area: search;
 }
-
-#center {
-  grid-area: center-panel;
-}
-
 
 .home {
   display: grid;
+  grid-template-areas: "header header search search search"
+                      "section-left . center-left center-right ."
+                      "section-left . center-left center-right ."
+                      ". . . . .";
   grid-template-columns: 2fr 2fr 2fr 2fr 2fr;
-  grid-template-areas: 
-  "";
+  grid-template-rows: 1fr 2fr 2fr 2fr;
+
+  
 }
+
+
 </style>
