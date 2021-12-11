c7c8a8f263efde833374f00abb591466f78cc6bb
Tarina Usher <tarinausher@gmail.com>
2021-12-10 19:55:58 -0500

    adjusted view/StudentProfile to link to component/StudentDetails.vue; endpoint /student/{id} should take user to specific student profile

--- frontend/src/components/StudentDetails.vue
+++ frontend/src/components/StudentDetails.vue
@@ -1,9 +1,17 @@
 <template>
   <div id='main'>
-      <div class='student-profile' >   
-          <p>{{ student}}</p> 
-          <p>Student Name: {{student.firstName}}&nbsp;{{student.lastName}}</p>
+      <div class='student-profile' v-bind:key="student.userId">   
+          <h1>{{student.firstName}}&nbsp;{{student.lastName}}</h1>
           <p>Cohort: {{student.cohortId}}</p>
+          <p>About: {{student.summary}}</p>
+          <p>Technical Skills: {{student.technologies}}</p>
+          <p>Soft Skills: {{student.softSkills}}</p>
+          <p>Education: TODO: IMPLEMENT DEGREE SPECIFIC COMPONENT TO POPULATE DEGREE DATA</p>
+          <p>Professional Experience: TODO: IMPLEMENT EXPERIENCE SPECIFIC COMPONENT TO POPULATE DEGREE DATA</p>
+          
+          <h2>Porfolio</h2>
+          <p>Portfolio Projects: TODO: IMPLEMENT PROJECT PORTFOLIO SPECIFIC COMPONENT</p>
+          <p>Probably some links, maybe the github external api, tbd</p>
       </div>
     </div>
 </template>
--- frontend/src/components/StudentList.vue
+++ frontend/src/components/StudentList.vue
@@ -1,6 +1,8 @@
 <template>
   <div id='main'>
+    <h1>Bootcamp Students</h1>
       <div class='students' v-for="student in students" v-bind:key="student.id" >    
+          
           <p>Student Name: {{student.firstName}}&nbsp;{{student.lastName}}</p>
           <p>Cohort: {{student.cohortId}}</p>
           <p>Technical Skills: {{student.technologies}}</p>
@@ -8,7 +10,7 @@
           <p>Portfolio Projects: TODO: BY TITLE <!-- will likely need a separate component for this--></p>
           
           <router-link v-bind:to="{ name: 'student-profile', params: { id: student.userId }}">
-          <button v-on:click="viewProfile(student.id)">View Profile</button>
+          <button>View Profile</button>
           </router-link>
           <br />
       </div>
@@ -31,9 +33,6 @@ export default {
         profileService.listStudents().then( (response) => {
           this.students = response.data;
         });
-      },
-      viewProfile(id) {
-        this.$router.push(`/student/${id}`);
       }
     },
     created() {
