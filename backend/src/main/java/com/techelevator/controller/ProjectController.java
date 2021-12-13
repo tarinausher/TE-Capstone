b4a28b898141a9b89b1554f6819e67b51a0e7c1a
Joseph <jbgriffithiv@gmail.com>
2021-12-12 20:34:41 -0500

    more API stuff

--- backend/src/main/java/com/techelevator/controller/ProjectController.java
+++ backend/src/main/java/com/techelevator/controller/ProjectController.java
@@ -2,13 +2,10 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.ProjectDao;
 import com.techelevator.model.Project;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -21,32 +18,32 @@ public class ProjectController {
     }
 
     @PostMapping(path = "/project")
-    void createProject(Project newProject) {
-
+    void createProject(Project updatedProject) {
+        projectDao.createProject(updatedProject);
     }
 
     @PutMapping(path = "/project/title")
     public void updateTitle(Project updatedProject) {
-
+        projectDao.updateTitle(updatedProject);
     }
 
     @PutMapping(path = "/project/description")
     void updateDescription(Project updatedProject) {
-
+        projectDao.updateDescription(updatedProject);
     }
 
     @PutMapping(path = "project/is-solo")
     void updateIsSolo(Project updatedProject) {
-
+        projectDao.updateIsSolo(updatedProject);
     }
 
     @PutMapping(path = "project/technologies")
     void updateTechnologies(Project updatedProject) {
-
+        projectDao.updateTechnologies(updatedProject);
     }
 
     @PutMapping(path = "project/link")
     void updateLink(Project updatedProject) {
-
+        projectDao.updateLink(updatedProject);
     }
 }
