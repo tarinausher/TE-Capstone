f0f1449c2aca8150f99b4b800fc0c0d244b94541
Joseph <jbgriffithiv@gmail.com>
2021-12-12 19:28:58 -0500

    more API stuff

--- backend/src/main/java/com/techelevator/controller/ProjectController.java
+++ backend/src/main/java/com/techelevator/controller/ProjectController.java
@@ -1,2 +1,52 @@
-package com.techelevator.controller;public class ProjectController {
+package com.techelevator.controller;
+
+import com.techelevator.dao.ProjectDao;
+import com.techelevator.model.Project;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class ProjectController {
+
+    private ProjectDao projectDao;
+
+    public ProjectController(ProjectDao projectDao) {
+        this.projectDao = projectDao;
+    }
+
+    @PostMapping(path = "/project")
+    void createProject(Project newProject) {
+
+    }
+
+    @PutMapping(path = "/project/title")
+    public void updateTitle(Project updatedProject) {
+
+    }
+
+    @PutMapping(path = "/project/description")
+    void updateDescription(Project updatedProject) {
+
+    }
+
+    @PutMapping(path = "project/is-solo")
+    void updateIsSolo(Project updatedProject) {
+
+    }
+
+    @PutMapping(path = "project/technologies")
+    void updateTechnologies(Project updatedProject) {
+
+    }
+
+    @PutMapping(path = "project/link")
+    void updateLink(Project updatedProject) {
+
+    }
 }
